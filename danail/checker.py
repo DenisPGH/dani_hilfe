b1f00bfec3a944476a18a3425675aa63c95ab1e1
Denislav <deni_9000_1990@abv.bg>
2022-11-07 10:33:40 +0100

    added func with 0, to match

--- danail/checker.py
+++ danail/checker.py
@@ -8,18 +8,34 @@ class Checker:
         self.RANGE_SMALL_NUMBERS=16
 
     def check_helper(self,vals:list):
+        """
+
+        :param vals: list allways with len ==8
+        :return: true if macht, false if not
+        """
         list_values=np.array(vals)
         numbers=np.array(list_values[:,0]).astype(int)
         colors=list_values[:,1]
         col,count = np.unique(colors, return_counts=True)
+        dict_compact=dict(zip(col,count))
+        if 'r' not in dict_compact.keys():
+            dict_compact['r']=0
+        if 'b' not in dict_compact.keys():
+            dict_compact['b']=0
+        if 'rb' not in dict_compact.keys():
+            dict_compact['rb']=0
+        #print(dict_compact)
 
-        if col[0]=='r' and count[0]==self.COUNT_OF_SAME_VALUES: # every are 'RED"
+
+        if col[0]=='r' and count[0]==self.COUNT_OF_SAME_VALUES or \
+            dict_compact['r']==self.COUNT_OF_SAME_VALUES-dict_compact['rb']: # every are 'RED"
             self.STATUS=True
-            self.MATCHING='RED'
+            self.MATCHING='RED' # work with 0
             return True
-        elif col[0]=='b' and count[0]==self.COUNT_OF_SAME_VALUES: # every are 'RED"
+        elif col[0]=='b' and count[0]==self.COUNT_OF_SAME_VALUES or \
+                dict_compact['b']==self.COUNT_OF_SAME_VALUES-dict_compact['rb']: # every are 'RED"
             self.STATUS=True
-            self.MATCHING = 'BLACK'
+            self.MATCHING = 'BLACK' # work with 0
             return True
 
         elif np.all([ True if int(a) > self.RANGE_SMALL_NUMBERS else False  for a in numbers ]): # every are 'BIG"
--- danail/read_data_winbet.py
+++ danail/read_data_winbet.py
@@ -5,8 +5,8 @@ def read_from_win_365():
     this function goes to the www.win365.com and get the value from the 17 rolets
     and return dict wit the last 8 values
     """
-    dict_={ 1:[(1,'r'),(9,'r'),(32,'r'),(0,'r'), (10,'r'),(19,'r'),(2,'r'),(20,'r')]} # red
-    dict_={ 1:[(1,'r'),(9,'r'),(32,'r'),(0,'r'), (10,'r'),(19,'r'),(2,'r'),(20,'b')]}
-    dict_={ 1:[(1,'r'),(7,'r'),(31,'r'),],2:[(19,'r')]} # odd
-    dict_={ 1:[(1,'r'),(0,'r'),(5,'r')]} # odd
+    dict_={ 1:[(1,'r'),(9,'r'),(32,'r'),(0,'rb'), (0,'rb'),(19,'r'),(2,'r'),(20,'r')]} # red
+    #dict_={ 1:[(1,'b'),(9,'b'),(32,'b'),(0,'rb'), (10,'b'),(19,'b'),(2,'b'),(20,'b')]}
+    #dict_={ 1:[(1,'r'),(7,'r'),(31,'r'),],2:[(19,'r')]} # odd
+    #dict_={ 1:[(1,'r'),(0,'rb'),(5,'r')]} # odd
     return dict_
