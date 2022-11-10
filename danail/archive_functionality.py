a9dfad8925cdf344406811617861c33416e18fd7
Denislav <deni_9000_1990@abv.bg>
2022-11-10 19:45:47 +0100

    added to compare only last 4 values, before store

--- danail/archive_functionality.py
+++ danail/archive_functionality.py
@@ -6,6 +6,7 @@ class DataBaseNumbers:
         self.time_= datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
         self.LAST_DICT_VALUES ={1:[],2:[],3:[]}
         self.path_store_rolete_info='db_store'
+        self.how_many_values_to_compare=4
     def store_new_info(self,dict_,time_):
         """
         store the every new line into db, if it a new number came
@@ -14,7 +15,8 @@ class DataBaseNumbers:
         :return:
         """
         for name,values in dict_.items():
-            if self.LAST_DICT_VALUES[name]==dict_[name]:
+            if self.LAST_DICT_VALUES[name][-self.how_many_values_to_compare:]==\
+                    dict_[name][-self.how_many_values_to_compare:]: # last four values
                 continue
             last_value=values[-1][0]
             last_value_color=values[-1][1]
--- danail/brack.py
+++ danail/brack.py
@@ -4,3 +4,6 @@ d={}
 
 if dict_2==dict_:
     print('a')
+
+a=[1,2,3,4,5,6]
+print(a[-2:])
