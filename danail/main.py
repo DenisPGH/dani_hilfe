76d6b81ff58914dc312f18e6047dcac310d7157a
Denislav <deni_9000_1990@abv.bg>
2022-11-15 18:49:37 +0100

    added linux comands

--- danail/main.py
+++ danail/main.py
@@ -4,6 +4,7 @@ from checker import Checker
 from image_recognition_rollete import ImageRecognition
 import datetime
 from speak_functionality import VoiceD
+import os
 check=Checker()
 db=DataBaseNumbers()
 ir=ImageRecognition()
@@ -15,15 +16,26 @@ print(st)
 # run this every 20 sec
 start_time=time.time()
 interval=2
+target_destiantion='/home/danailparvanov/Desktop/Casino/test'
+source_destination='/home/danailparvanov/Pictures'
 
 while True:
     cur_time=time.time()
     if cur_time-start_time>=interval:
         t = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+        ########### make screenshot
+        # del all in target
+        del_t = f"rm {target_destiantion}/screen.png"
+        os.system(del_t)
         # make screenshot
+        scr_make = 'gnome-screenshot'
+        os.system(scr_make)
+        # move and rename to my folder
+        move_ = f"mv {source_destination}/*.png {target_destiantion}/screen.png"
+        os.system(move_)
 
-        # source_file
-        source_file='screen_/scr_1.png'
+        # ############take source_file
+        source_file=f'{target_destiantion}/screen.png'
         # data from reader
         result=ir.main_function(source_file)
         # check for win
@@ -31,4 +43,11 @@ while True:
         # store into database
         db.store_new_info(result,t)
         start_time=cur_time
+
+
+
+
+
+
+
 
